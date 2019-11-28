59f17f202cbcddb5f500eb2dbc5e085f7e051ee3
dwiszyns <dominikwisza@gmail.com>
2019-11-28 11:20:31 +0100

    Fixed views

--- Controllers/NoteController.cs
+++ Controllers/NoteController.cs
@@ -226,7 +226,7 @@ namespace NTR2.Controllers
                             if (databaseValues.NoteDate != clientValues.NoteDate)
                                 ModelState.AddModelError("NoteDate", "Current value: "
                                     + String.Format("{0:d}", databaseValues.NoteDate));
-                            ModelState.AddModelError(string.Empty, "The record you attempted to edit "
+                            ModelState.AddModelError("Modified", "The record you attempted to edit "
                                 + "was modified by another user after you got the original value. The "
                                 + "edit operation was canceled and the current values in the database "
                                 + "have been displayed. If you still want to edit this record, click "
